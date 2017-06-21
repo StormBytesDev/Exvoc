1628dea0a741ccd9a98938accd577495c74f4b96
Edu Renesto <edure95@gmail.com>
2017-06-21 18:11:52 +0000

    Add copyright notice

--- src/main/java/com/stormbytes/exvoc/client/ClientProxy.java
+++ src/main/java/com/stormbytes/exvoc/client/ClientProxy.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Exvoc.
+ *
+ * Exvoc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exvoc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Exvoc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.stormbytes.exvoc.client;
 
 import com.stormbytes.exvoc.common.CommonProxy;
